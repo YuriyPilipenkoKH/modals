448f38f335965014fe76be23b6f1a23572cf817e
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-09 13:44:55 +0200

    q

--- js/modal-one.js
+++ js/modal-one.js
@@ -1,25 +1,62 @@
+
+const modals = {
+     modalFirst : document.querySelector('.popup__bg-1'),
+     modalSecod : document.querySelector('.popup__bg-2'),
+     modalThird : document.querySelector('.popup__bg-3'),
+     modalFourth: document.querySelector('.popup__bg-4'),
+     modalFifth : document.querySelector('.popup__bg-5'),
+
+     popupsArray: document.querySelectorAll('.popup__collection li'),
+     openButtonsArray: document.querySelectorAll('#m-1, #m-2, #m-3, #m-4, #m-5'),
+     closeButtonsArray: document.querySelectorAll('.close-popup'),
+
+}
+
+console.log(modals.popupsArray);
+console.log(modals.openButtonsArray);
+console.log(modals.closeButtonsArray);
+
+// modals.modalThird.classList.add('active')
+// console.log(modals.popupsArray[0])
+
+modals.openButtonsArray.forEach((button) => {
+    console.log(button.textContent);
+    button.addEventListener('click', (e) => {
+        let i = 3
+        e.preventDefault();
+       
+        modals.popupsArray[i].classList.add('active')
+    })
+    })
+
+
 const refs = {
     openModalBtn: document.querySelector('[data-action="open-modal"]') ,
     closeModalBtn: document.querySelector('.button-close') ,
     closeSecondModalBtn: document.querySelector('.button-close-2') ,
-
     openSecondModalBtn: document.querySelector('[data-action="open-modal-2"]') ,
     secondBackdrop: document.querySelector('.backdrop-2'),
-
     backdrop: document.querySelector('.js-backdrop'),
-};
+
+}
 
  refs.openModalBtn.addEventListener('click',onOpenModal);
  refs.closeModalBtn.addEventListener('click',onCloseModal);
  refs.closeSecondModalBtn.addEventListener('click',onCloseModal);
  refs.backdrop.addEventListener('click',onBackdropClick);
 
-//  mod2
- refs.openSecondModalBtn.addEventListener('click',onOpenModal2);
 
-function onOpenModal() {
+
+
+
+function onOpenModal(e) {
+    e.preventDefault();
     window.addEventListener('keydown', onEscPress)
     document.body.classList.add('show-modal');
+   
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+
 }  
 
 function onOpenModal2() {
--- js/modal-two.js
+++ js/modal-two.js
@@ -1,7 +1,7 @@
 let popupBg = document.querySelector('.popup__bg-1');
 let popup = document.querySelector('.popup-1');
-let openPopupButtons = document.querySelector('.open-popup');
-let closePopupButtons = document.querySelector('.close-popup');
+let openPopupButtons = document.querySelectorAll('.open-popup-1');
+let closePopupButtons = document.querySelector('.close-popup-1');
 
 openPopupButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
@@ -12,9 +12,25 @@ openPopupButtons.forEach((button) => {
 });
 
 closePopupButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        // e.preventDefault();
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
         popupBg.classList.remove('active');
         popup.classList.remove('active');
     })
 });
+
+
+    document.addEventListener('click', (e) => {
+        if(e.target === popupBg) {
+            popupBg.classList.remove('active');
+            popup.classList.remove('active');
+        }
+    
+    })
+
+    // function onEscPress(e) {
+    //     if (e.code === 'Escape') {
+    //         popupBg.classList.remove('active');
+    //         popup.classList.remove('active');
+    //     }
+    // }    
