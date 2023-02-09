10f22be5145d448a8579bfdc32f557e427c7c588
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-09 23:39:43 +0200

    a

--- js/modal-two.js
+++ js/modal-two.js
@@ -1,14 +1,17 @@
 
 const container = document.querySelector('.button__list');
 const closeBtn = document.querySelector('.popup__collection');
-const backdrop = document.querySelectorAll('.popup__collection > li');
-console.log(backdrop);
+// const backdrop = document.querySelectorAll('.popup__collection > li');
+const closeArray = document.querySelectorAll('.close-popup')
+console.log(closeArray);
+
 
 container.addEventListener('click', onClick) ;
 closeBtn.addEventListener('click', onCloseModal) ;
 
 
 function onClick(event) {
+    // window.addEventListener('keydown',OnEscKey)
     let k = event.target.id
     // let back = `popupBg${k}`
     let back = document.getElementById(`p${k}`)
@@ -27,18 +30,22 @@ function onClick(event) {
 
 
 function onCloseModal(event) {
+    // window.removeEventListener('keydown',OnEscKey)
     let currentActive = document.querySelector('.active')
-
-    currentActive.classList.remove('active')
-}
-
-function OnEscKey(event) {
-    const ESC = 'Escape'
-    const isEscape = event.code === ESC
-
-    if(isEscape) {
-        onCloseModal()
+    if (event.target.nodeName !== 'BUTTON') {
+        return
     }
+    currentActive.classList.remove('active')
+  
 }
+
+// function OnEscKey(event) {
+//     const ESC = 'Escape'
+//     const isEscape = event.code === ESC
+
+//     if(isEscape) {
+//         onCloseModal()
+//     }
+// }
 
 
