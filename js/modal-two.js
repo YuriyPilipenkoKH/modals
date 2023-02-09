f4e7f105c7c686d536bccbbd17171296196207a6
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-09 17:09:36 +0200

    q

--- js/modal-two.js
+++ js/modal-two.js
@@ -1,32 +1,54 @@
-let popupBg = document.querySelector('.popup__bg-1');
-let popup = document.querySelector('.popup-1');
-let openPopupButtons = document.querySelectorAll('.open-popup-1');
-let closePopupButtons = document.querySelector('.close-popup-1');
 
-openPopupButtons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        popupBg.classList.add('active');
-        popup.classList.add('active');
-    })
-});
+const closeBtn = document.getElementById("close")
 
-closePopupButtons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        popupBg.classList.remove('active');
-        popup.classList.remove('active');
-    })
-});
+const container = document.querySelector('.modal-two');
+container.addEventListener('click', onClick)
+
+function onClick(event) {
+    let k = event.target.id
+    // let back = `popupBg${k}`
+    let back = document.getElementById(`p${k}`)
+    if (event.target.nodeName !== 'BUTTON') {
+        return
+    }
+
+     event.preventDefault();
+     back.classList.add('active');
+   
+
+    console.log(event.target);
+    // console.log(event.target.textContent);
+    // console.log(event.target.nodeName);
+    console.log(back);
+
+}
 
 
-    document.addEventListener('click', (e) => {
-        if(e.target === popupBg) {
-            popupBg.classList.remove('active');
-            popup.classList.remove('active');
-        }
+
+// openPopupButtons.forEach((button) => {
+//     button.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         popupBg.classList.add('active');
+//         popup.classList.add('active');
+//     })
+// });
+
+// closePopupButtons.forEach((button) => {
+//     button.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         popupBg.classList.remove('active');
+//         popup.classList.remove('active');
+//     })
+// });
+
+
+//     document.addEventListener('click', (e) => {
+//         if(e.target === popupBg) {
+//             popupBg.classList.remove('active');
+//             popup.classList.remove('active');
+//         }
     
-    })
+//     })
 
     // function onEscPress(e) {
     //     if (e.code === 'Escape') {
