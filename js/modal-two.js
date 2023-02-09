3c36e14366297cdc458beda9281682b5bf2128cd
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-10 00:32:06 +0200

    esc ?

--- js/modal-two.js
+++ js/modal-two.js
@@ -1,17 +1,15 @@
 
 const container = document.querySelector('.button__list');
 const closeBtn = document.querySelector('.popup__collection');
-// const backdrop = document.querySelectorAll('.popup__collection > li');
+const backdrop = document.querySelectorAll('.popup__collection > li');
 const closeArray = document.querySelectorAll('.close-popup')
-console.log(closeArray);
-
 
 container.addEventListener('click', onClick) ;
 closeBtn.addEventListener('click', onCloseModal) ;
 
 
 function onClick(event) {
-    // window.addEventListener('keydown',OnEscKey)
+    window.addEventListener('keydown',OnEscKey)
     let k = event.target.id
     // let back = `popupBg${k}`
     let back = document.getElementById(`p${k}`)
@@ -20,7 +18,6 @@ function onClick(event) {
     }
      event.preventDefault();
      back.classList.add('active');
-
     // console.log(event.target);
     // console.log(event.target.textContent);
     // console.log(event.target.nodeName);
@@ -30,22 +27,25 @@ function onClick(event) {
 
 
 function onCloseModal(event) {
-    // window.removeEventListener('keydown',OnEscKey)
-    let currentActive = document.querySelector('.active')
-    if (event.target.nodeName !== 'BUTTON') {
-        return
+    window.removeEventListener('keydown',OnEscKey)
+    console.log(event.target.id);
+    console.log(event.target.nodeName);
+    if (event.target.id === 'close') {
+        backdrop.forEach(el => {
+            el.classList.remove('active')
+        });
     }
-    currentActive.classList.remove('active')
-  
 }
 
-// function OnEscKey(event) {
-//     const ESC = 'Escape'
-//     const isEscape = event.code === ESC
+function OnEscKey(event) {
+    const ESC = 'Escape'
+    const isEscape = event.code === ESC
 
-//     if(isEscape) {
-//         onCloseModal()
-//     }
-// }
+    if(isEscape) {
+        backdrop.forEach(el => {
+            el.classList.remove('active')
+        });
+    }
+}
 
 
